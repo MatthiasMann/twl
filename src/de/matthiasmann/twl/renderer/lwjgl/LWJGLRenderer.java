700639fa80098178d579011085651475eada8c29
Matthias Mann <devnull@localhost>
2009-07-19 22:54:51 +0200

    disable depth test and lighting for UI rendering

--- src/de/matthiasmann/twl/renderer/lwjgl/LWJGLRenderer.java
+++ src/de/matthiasmann/twl/renderer/lwjgl/LWJGLRenderer.java
@@ -151,6 +151,8 @@ public class LWJGLRenderer implements Renderer, LineRenderer {
         GL11.glEnable(GL11.GL_TEXTURE_2D);
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glEnable(GL11.GL_LINE_SMOOTH);
+        GL11.glDisable(GL11.GL_DEPTH_TEST);
+        GL11.glDisable(GL11.GL_LIGHTING);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
         GL11.glHint(GL11.GL_LINE_SMOOTH_HINT, GL11.GL_NICEST);
     }
