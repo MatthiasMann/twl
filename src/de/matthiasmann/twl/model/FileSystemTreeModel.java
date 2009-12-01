1b916c1bd5203489d68caef31f61fc29511230e6
Matthias Mann <devnull@localhost>
2009-12-02 00:02:13 +0100

    added support for last modified column

--- src/de/matthiasmann/twl/model/FileSystemTreeModel.java
+++ src/de/matthiasmann/twl/model/FileSystemTreeModel.java
@@ -1,30 +1,70 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2008-2009, Matthias Mann
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Matthias Mann nor the names of its contributors may
+ *       be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package de.matthiasmann.twl.model;
 
+import java.util.Date;
+
 /**
+ * A tree model which displays hthe folders of a FileSystemModel
  *
- * @author mam
+ * @author Matthias Mann
  */
 public class FileSystemTreeModel extends AbstractTreeTableModel {
 
     private final FileSystemModel fsm;
+    private final boolean includeLastModified;
 
-    public FileSystemTreeModel(FileSystemModel fsm) {
+    public FileSystemTreeModel(FileSystemModel fsm, boolean includeLastModified) {
         this.fsm = fsm;
+        this.includeLastModified = includeLastModified;
 
         insertRoots();
     }
 
+    public FileSystemTreeModel(FileSystemModel fsm) {
+        this(fsm, false);
+    }
+
     public int getNumColumns() {
-        return 1;
+        return includeLastModified ? 2 : 1;
     }
 
     public String getColumnHeaderText(int column) {
-        return "Folder";
+        switch(column) {
+            case 0:
+                return "Folder";
+            case 1:
+                return "Last modified";
+            default:
+                return "";
+        }
     }
 
     public FileSystemModel getFileSystemModel() {
@@ -77,7 +117,14 @@ public class FileSystemTreeModel extends AbstractTreeTableModel {
         }
 
         public Object getData(int column) {
-            return fsm.getName(folder);
+            switch(column) {
+                case 0:
+                    return fsm.getName(folder);
+                case 1:
+                    return new Date(fsm.getLastModified(folder));
+                default:
+                    return null;
+            }
         }
 
         public TreeTableNode getChild(int idx) {
