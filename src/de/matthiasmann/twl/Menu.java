1f1903460d87b156921cda8758f3564847b47ca1
Matthias Mann <devnull@localhost>
2010-04-29 18:45:36 +0200

    Menu: added javadoc

--- src/de/matthiasmann/twl/Menu.java
+++ src/de/matthiasmann/twl/Menu.java
@@ -34,6 +34,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 /**
+ * A menu which can be displayed as menu bar or as popup menu.
  *
  * @author Matthias Mann
  */
@@ -44,64 +45,134 @@ public class Menu extends MenuElement implements Iterable<MenuElement> {
     private final ArrayList<MenuElement> elements = new ArrayList<MenuElement>();
     private String popupTheme;
 
+    /**
+     * Creates a new menu without name.
+     * This constructor should be used for top level menus.
+     *
+     * @see #createMenuBar()
+     * @see #createMenuBar(de.matthiasmann.twl.Widget)
+     * @see #openPopupMenu(de.matthiasmann.twl.Widget)
+     * @see #openPopupMenu(de.matthiasmann.twl.Widget, int, int)
+     */
     public Menu() {
     }
 
+    /**
+     * Creates a new menu with the given name.
+     * This constructor should be used used for sub menus. The name is used for
+     * the button which opens this sub menu.
+     *
+     * @param name The name of the popup menu entry
+     * @see #add(de.matthiasmann.twl.MenuElement)
+     */
     public Menu(String name) {
         super(name);
     }
 
+    /**
+     * Returns the theme which is used when this menu is displayed as popup/sub menu.
+     * @return the popup theme
+     */
     public String getPopupTheme() {
         return popupTheme;
     }
 
+    /**
+     * Sets the theme which is used when this menun is displayed as popup/sub menu.
+     * @param popupTheme the popup theme
+     */
     public void setPopupTheme(String popupTheme) {
         String oldPopupTheme = this.popupTheme;
         this.popupTheme = popupTheme;
         firePropertyChange("popupTheme", oldPopupTheme, this.popupTheme);
     }
 
+    /**
+     * Returns a mutable iterator which iterators over all menu elements
+     * @return a iterator
+     */
     public Iterator<MenuElement> iterator() {
         return elements.iterator();
     }
 
+    /**
+     * Returns the menu element at the given index.
+     * @param index the index. Must be &lt; {code getNumElements}
+     * @return the menu element
+     * @throws IndexOutOfBoundsException if index is invalid
+     * @see #getNumElements()
+     */
     public MenuElement get(int index) {
         return elements.get(index);
     }
 
+    /**
+     * Returns the number of menu elements in this menu.
+     * @return the number of menu elements
+     */
     public int getNumElements() {
         return elements.size();
     }
-    
+
+    /**
+     * Removes all menu elements
+     */
     public void clear() {
         elements.clear();
     }
 
+    /**
+     * Adds the given menu element at the end. It is possible to add the same
+     * menu element several times also in different menus.
+     *
+     * @param e the menu element
+     * @return this
+     */
     public Menu add(MenuElement e) {
         elements.add(e);
         return this;
     }
 
+    /**
+     * Adds a {code MenuAction} element at the end. It is equivalent to
+     * {code add(new MenuAction(name, cb)) }
+     *
+     * @param name the name of the menu action
+     * @param cb the callback when the menu action has been selected
+     * @return this
+     */
     public Menu add(String name, Runnable cb) {
         return add(new MenuAction(name, cb));
     }
 
+    /**
+     * Adds a {code MenuCheckbox} element at the end.  It is equivalent to
+     * {code add(new MenuCheckbox(name, model)) }
+     *
+     * @param name the name of the menu checkbox
+     * @param model the boolean model which is displayed/modified by the menu checkbox
+     * @return this
+     */
     public Menu add(String name, BooleanModel model) {
         return add(new MenuCheckbox(name, model));
     }
-    
+
+    /**
+     * Adds a {code MenuSpacer} element at the end.  It is equivalent to
+     * {code add(new MenuSpacer()) }
+     *
+     * @return this
+     */
     public Menu addSpacer() {
         return add(new MenuSpacer());
     }
-    
-    @Override
-    protected Widget createMenuWidget(MenuManager mm, int level) {
-        SubMenuBtn smb = new SubMenuBtn(mm, level);
-        smb.setEnabled(isEnabled());
-        setWidgetTheme(smb, "submenu");
-        return smb;
-    }
 
+    /**
+     * Creates a menu bar by adding all menu widgets to the specified container.
+     *
+     * @param container the container for the menu widgets.
+     * @see #createMenuBar()
+     */
     public void createMenuBar(Widget container) {
         MenuManager mm = new MenuManager(container, true);
         for(Widget w : createWidgets(mm, 0)) {
@@ -109,6 +180,12 @@ public class Menu extends MenuElement implements Iterable<MenuElement> {
         }
     }
 
+    /**
+     * Creates a menu bar with a DialogLayout as conatiner. This is the preferred
+     * method to create a menu bar.
+     *
+     * @return the menu bar conatiner
+     */
     public Widget createMenuBar() {
         DialogLayout l = new DialogLayout();
         setWidgetTheme(l, "menubar");
@@ -123,12 +200,29 @@ public class Menu extends MenuElement implements Iterable<MenuElement> {
         return l;
     }
 
+    /**
+     * Creates a popup menu from this menu. The popup is positioned to the right of
+     * the parent widget.
+     *
+     * @param parent the parent widget for the popup.
+     * @return the MenuManager which manages this popup
+     * @see MenuManager#closePopup() 
+     */
     public MenuManager openPopupMenu(Widget parent) {
         MenuManager mm = new MenuManager(parent, false);
         mm.openSubMenu(0, this, parent, true);
         return mm;
     }
-    
+
+    /**
+     * Creates a popup menu from this menu at the specified position.
+     *
+     * @param parent the parent widget for the popup.
+     * @param x the absolute X coordinate for the popup
+     * @param y the absolute Y coordinate for the popup
+     * @return the MenuManager which manages this popup
+     * @see MenuManager#closePopup()
+     */
     public MenuManager openPopupMenu(Widget parent, int x, int y) {
         MenuManager mm = new MenuManager(parent, false);
         Widget popup = mm.openSubMenu(0, this, parent, false);
@@ -136,6 +230,13 @@ public class Menu extends MenuElement implements Iterable<MenuElement> {
             popup.setPosition(x, y);
         }
         return mm;
+    }
+
+    @Override
+    protected Widget createMenuWidget(MenuManager mm, int level) {
+        SubMenuBtn smb = new SubMenuBtn(mm, level);
+        setWidgetTheme(smb, "submenu");
+        return smb;
     }
 
     private Widget[] createWidgets(MenuManager mm, int level) {
