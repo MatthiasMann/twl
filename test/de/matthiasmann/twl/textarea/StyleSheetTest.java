1dcf061f4437541b6e15d77e0a2fccc4f63bf4be
Matthias Mann <devnull@localhost>
2012-01-22 11:47:37 +0100

    missed test case for last commit

--- test/de/matthiasmann/twl/textarea/StyleSheetTest.java
+++ test/de/matthiasmann/twl/textarea/StyleSheetTest.java
@@ -1,8 +1,32 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2008-2012, Matthias Mann
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Matthias Mann nor the names of its contributors may
+ *       be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package de.matthiasmann.twl.textarea;
 
 import java.io.StringReader;
@@ -10,6 +34,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import static de.matthiasmann.twl.textarea.StyleAttribute.*;
 import static de.matthiasmann.twl.textarea.Value.Unit.*;
+import de.matthiasmann.twl.utils.StringList;
 
 /**
  *
@@ -30,38 +55,38 @@ public class StyleSheetTest {
     public void testParse2() throws Exception {
         StyleSheet ss = new StyleSheet();
         ss.parse(new StringReader("a { font: link }"));
-        test(ss, FONT_NAME, "link", new StyleSheetKey("a", null, null));
-        test(ss, FONT_NAME, "default", new StyleSheetKey("td", null, null));
+        test(ss, FONT_FAMILIES, new StringList("link"), new StyleSheetKey("a", null, null));
+        test(ss, FONT_FAMILIES, new StringList("default"), new StyleSheetKey("td", null, null));
     }
 
     @Test
     public void testParse3() throws Exception {
         StyleSheet ss = new StyleSheet();
         ss.parse(new StringReader("a { font: link; }"));
-        test(ss, FONT_NAME, "link", new StyleSheetKey("a", null, null));
-        test(ss, FONT_NAME, "default", new StyleSheetKey("td", null, null));
+        test(ss, FONT_FAMILIES, new StringList("link"), new StyleSheetKey("a", null, null));
+        test(ss, FONT_FAMILIES, new StringList("default"), new StyleSheetKey("td", null, null));
     }
 
     @Test
     public void testParse4() throws Exception {
         StyleSheet ss = new StyleSheet();
         ss.parse(new StringReader("div a { font: link; }"));
-        test(ss, FONT_NAME, "default", new StyleSheetKey("a", null, null));
-        test(ss, FONT_NAME, "default", new StyleSheetKey("div", null, null));
-        test(ss, FONT_NAME, "default", new StyleSheetKey("td", null, null));
-        test(ss, FONT_NAME, "link", new StyleSheetKey("div", null, null), new StyleSheetKey("a", null, null));
-        test(ss, FONT_NAME, "link", new StyleSheetKey("div", null, null), new StyleSheetKey("p", null, null), new StyleSheetKey("a", null, null));
+        test(ss, FONT_FAMILIES, new StringList("default"), new StyleSheetKey("a", null, null));
+        test(ss, FONT_FAMILIES, new StringList("default"), new StyleSheetKey("div", null, null));
+        test(ss, FONT_FAMILIES, new StringList("default"), new StyleSheetKey("td", null, null));
+        test(ss, FONT_FAMILIES, new StringList("link"), new StyleSheetKey("div", null, null), new StyleSheetKey("a", null, null));
+        test(ss, FONT_FAMILIES, new StringList("link"), new StyleSheetKey("div", null, null), new StyleSheetKey("p", null, null), new StyleSheetKey("a", null, null));
     }
 
     @Test
     public void testParse5() throws Exception {
         StyleSheet ss = new StyleSheet();
         ss.parse(new StringReader("div > a { font: link; }"));
-        test(ss, FONT_NAME, "default", new StyleSheetKey("a", null, null));
-        test(ss, FONT_NAME, "default", new StyleSheetKey("div", null, null));
-        test(ss, FONT_NAME, "default", new StyleSheetKey("td", null, null));
-        test(ss, FONT_NAME, "link", new StyleSheetKey("div", null, null), new StyleSheetKey("a", null, null));
-        test(ss, FONT_NAME, "default", new StyleSheetKey("div", null, null), new StyleSheetKey("p", null, null), new StyleSheetKey("a", null, null));
+        test(ss, FONT_FAMILIES, new StringList("default"), new StyleSheetKey("a", null, null));
+        test(ss, FONT_FAMILIES, new StringList("default"), new StyleSheetKey("div", null, null));
+        test(ss, FONT_FAMILIES, new StringList("default"), new StyleSheetKey("td", null, null));
+        test(ss, FONT_FAMILIES, new StringList("link"), new StyleSheetKey("div", null, null), new StyleSheetKey("a", null, null));
+        test(ss, FONT_FAMILIES, new StringList("default"), new StyleSheetKey("div", null, null), new StyleSheetKey("p", null, null), new StyleSheetKey("a", null, null));
     }
 
     @Test
